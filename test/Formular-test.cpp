c9463abd594da998edfe1114a6b163f643adc135
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2021-07-13 13:59:48 +0200

    Add first test case

--- test/Formular-test.cpp
+++ test/Formular-test.cpp
@@ -1,8 +1,11 @@
+#include "Formular.h"
 #include "gtest/gtest.h"
 
 TEST(blaTest, test1) {
     //arrange
     //act
     //assert
-    EXPECT_EQ ((0),  0);
+    EXPECT_EQ (Formular::bla(0), 0);
+    EXPECT_EQ (Formular::bla(10), 20);
+    EXPECT_EQ (Formular::bla(50), 100);
 }
